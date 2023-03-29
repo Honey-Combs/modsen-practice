4528415f46e7b631f3a51e8637e7b3bcebf80eae
Honey-Combs <98231087+Honey-Combs@users.noreply.github.com>
2023-03-29 19:04:02 +0300

    fix: fix layout styles

--- src/layouts/PageLayout/styled.ts
+++ src/layouts/PageLayout/styled.ts
@@ -1,10 +1,19 @@
 import styled from 'styled-components';
+import img1 from '@assets/img1.jpg';
 
 export const StyledPageLayout = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  min-height: 100vh;
+  width: 100%;
+  overflow-y: auto;
   padding: 4rem;
+  background: url(${img1});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-attachment: fixed;
 `;
