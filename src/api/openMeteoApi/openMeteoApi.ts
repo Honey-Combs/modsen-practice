3f7fc39440e263251e4c2fbcd3f89fcfd42857f5
Honey-Combs <98231087+Honey-Combs@users.noreply.github.com>
2023-04-11 23:25:59 +0300

    fix: fixed api

---
+++ src/api/openMeteoApi/openMeteoApi.ts
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import { IOpenMeteoDailyForecast, IOpenMeteoHourlyForecast } from './responseModels';
+import { GPSCoordinates } from '@/typings/types/GPSCoordinates';
+
+export interface IOpenMeteoApi {
+  getDailyForecast: ({ latitude, longitude }: GPSCoordinates) => Promise<IOpenMeteoDailyForecast>;
+  getHourlyForecast: ({ latitude, longitude }: GPSCoordinates) => Promise<IOpenMeteoHourlyForecast>;
+}
+
+const BASE_URL = 'https://api.open-meteo.com/v1';
+
+async function getDailyForecast<IOpenMeteoDailyForecast>({ latitude, longitude }: GPSCoordinates) {
+  const { data } = await axios.get<IOpenMeteoDailyForecast>(
+    `${BASE_URL}/forecast?latitude=${latitude}&longitude=${longitude}&forecast_days=16&timezone=auto&daily=temperature_2m_max,temperature_2m_min,weathercode,winddirection_10m_dominant,windspeed_10m_max`
+  );
+  
+  return data;
+}
+
+async function getHourlyForecast({ latitude, longitude }: GPSCoordinates) {
+  const response = await fetch(
+    `${BASE_URL}/forecast?latitude=${latitude}
+  &longitude=${longitude}
+  &hourly=temperature_2m,weathercode`
+  );
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+
+  return response.json() as Promise<IOpenMeteoHourlyForecast>;
+}
+
+export const OpenMeteoApi: IOpenMeteoApi = {
+  getDailyForecast,
+  getHourlyForecast,
+};
---
+++ src/api/openMeteoApi/responseModels.ts
@@ -0,0 +1,47 @@
+export interface IOpenMeteoDailyForecast
+  extends IBasicParameters {
+  daily_units: {
+    time: string;
+    temperature_2m_max: string;
+    temperature_2m_min: string;
+    weathercode: string;
+    winddirection_10m_dominant: string;
+    windspeed_10m_max: string;
+  };
+  daily: {
+    time: string[];
+    temperature_2m_max: number[];
+    temperature_2m_min: number[];
+    weathercode: number[];
+    winddirection_10m_dominant: number[];
+    windspeed_10m_max: number[];
+  };
+}
+
+export interface IOpenMeteoHourlyForecast
+  extends IBasicParameters {
+  hourly_units: {
+    time: string;
+    temperature_2m: string;
+    weathercode: string;
+    winddirection_10m: string;
+    windspeed_10m: string;
+  };
+  daily: {
+    time: string[];
+    temperature_2m: number[];
+    weathercode: number[];
+    winddirection_10m: number[];
+    windspeed_10m: number[];
+  };
+}
+
+interface IBasicParameters {
+  latitude: number;
+  longitude: number;
+  generationtime_ms: number;
+  utc_offset_seconds: number;
+  timezone: string;
+  timezone_abbreviation: string;
+  elevation: number;
+}
