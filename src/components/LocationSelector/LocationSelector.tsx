91b7d649aa7ef35e668c3e1c1538a29f886686a6
Honey-Combs <98231087+Honey-Combs@users.noreply.github.com>
2023-04-13 23:12:46 +0300

    feat: added locationSelector

--- src/components/LocationSelector/LocationSelector.tsx
+++ src/components/LocationSelector/LocationSelector.tsx
@@ -1,10 +1,48 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import React from 'react';
+import Select from 'react-select';
+import { useForm, Controller, SubmitHandler } from 'react-hook-form';
 import { StyledLocationSelector } from '@/components/LocationSelector/styled';
 
+type FormData = {
+  cityName: string;
+};
+
+type City = {
+  label: string;
+  value: string;
+};
+
 export function LocationSelector() {
+  const { handleSubmit, control } = useForm<FormData>();
+  const countries = [
+    { label: 'Mogilev', value: 'Mogilev' },
+    { label: 'Minsk', value: 'Minsk' },
+  ];
+
+  const onSubmit: SubmitHandler<FormData> = ({ cityName }: FormData) => {
+    console.log(cityName);
+  };
+
   return (
     <StyledLocationSelector>
-      LocationSelector
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          name="cityName"
+          control={control}
+          render={({field: {onChange}}) => (
+            <Select
+              options={countries}
+              onChange={(selectedOption: City | null) => {
+                onChange(selectedOption?.value);
+              }}
+            />
+          )}
+          rules={{ required: true }}
+        />
+      </form>
     </StyledLocationSelector>
   );
 }
--- src/components/LocationSelector/styled.ts
+++ src/components/LocationSelector/styled.ts
@@ -5,4 +5,5 @@ export const StyledLocationSelector = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  color: black;
 `;
